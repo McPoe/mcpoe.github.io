bc72b89c7166a71cf88af0bcf1f4beb4cfdc1a0a
Maciel Porraz <maciel.porraz@gmail.com>
2021-03-22 21:43:26 -0700

    Last changes
    Finally almost all

---
+++ js/transitions.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+	$(".btnweb").css("opacity", "0%");
+  	
+//each web image
+  	$("#cb1").mouseenter(function(){
+        $("#imgweb1").css("opacity", "10%");
+        $("#btn1").show().css("opacity", "100%");
+    });
+    $("#cb1").mouseleave(function(){
+        $("#imgweb1").css("opacity", "100%");
+        $("#btn1").css("opacity", "0%");
+    });
+
+
+    //each web image
+  	$("#cb2").mouseenter(function(){
+        $("#imgweb2").css("opacity", "10%");
+        $("#btn2").show().css("opacity", "100%");
+    });
+    $("#cb2").mouseleave(function(){
+        $("#imgweb2").css("opacity", "100%");
+        $("#btn2").css("opacity", "0%");
+    });
+
+
+    //each web image
+  	$("#cb3").mouseenter(function(){
+        $("#imgweb3").css("opacity", "10%");
+        $("#btn3").show().css("opacity", "100%");
+    });
+    $("#cb3").mouseleave(function(){
+        $("#imgweb3").css("opacity", "100%");
+        $("#btn3").css("opacity", "0%");
+    });
+
+
+    //each web image
+  	$("#cb4").mouseenter(function(){
+        $("#imgweb4").css("opacity", "10%");
+        $("#btn4").show().css("opacity", "100%");
+    });
+    $("#cb4").mouseleave(function(){
+        $("#imgweb4").css("opacity", "100%");
+        $("#btn4").css("opacity", "0%");
+    });
+
+
+    //each web image
+  	$("#cb5").mouseenter(function(){
+        $("#imgweb5").css("opacity", "10%");
+        $("#btn5").show().css("opacity", "100%");
+    });
+    $("#cb5").mouseleave(function(){
+        $("#imgweb5").css("opacity", "100%");
+        $("#btn5").css("opacity", "0%");
+    });
+
+
+    //each web image
+  	$("#cb6").mouseenter(function(){
+        $("#imgweb6").css("opacity", "10%");
+        $("#btn6").show().css("opacity", "100%");
+    });
+    $("#cb6").mouseleave(function(){
+        $("#imgweb6").css("opacity", "100%");
+        $("#btn6").css("opacity", "0%");
+    });
+
+});
+
+
+
+ 
